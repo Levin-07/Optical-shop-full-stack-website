6c013f03c5db9159309e104f61eeab7f8935bcef
{Levin} <{levintjose@gmail.com}>
2026-04-21 00:35:14 +0530

    Fix Express 5 routing error for React fallback

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const path = require("path");
 // Serve Frontend in Production
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
 });
 
